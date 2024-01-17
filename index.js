02ab3396162c027375be627ba2348163d730c4c5
Jyrone Parker <jyrone.parker@atlasied.com>
2024-01-16 21:48:48 -0500

    test

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import { Camera } from "./src/classes/Camera";
 import { VideoTransformer } from "./src/classes/transformers/VideoTransformer";
 import { StoreFrames } from "./src/classes/transformers/StoreFrames";
 
 export {
-  Camera,
   VideoTransformer,
   StoreFrames
 }
